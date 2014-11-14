09659ce347a9a601dfd5e3be69c17402d709e16d
zhangsq <zhangsq@outlook.com>
2014-11-14 18:10:48 +0800

    '增加任务模型'

---
+++ models/Task.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/workLog');
+
+var schema = new mongoose.Schema({
+    name : {type : String},
+    jira : {type : String},
+    start : {type : Date, default : new Date()},
+    end : {type : Date, default : null},
+    stat : {type : Number, default : 1},
+    progress : {type : Number, default : 0},
+    uid : {type : String},
+    remark : {type : Array, default : []}
+});
+
+module.exports = mongoose.model('Task', schema, 'tasks');
